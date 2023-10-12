41bd74843b21bd79275a6328fb40d638b21cfecd
nobytes <93953223+nobytesguy@users.noreply.github.com>
2023-10-12 01:20:46 -0300

    feat: new way of reading algorithms

--- cmd/sanae/main.go
+++ cmd/sanae/main.go
@@ -7,15 +7,8 @@ import (
 )
 
 func main() {
-	firstCmd := sanae.NewCommand(1, "write 2 mover", 1)
-	secondCmd := sanae.NewCommand(2, "write 4 mover", 1)
-	firstState := sanae.NewState([]sanae.Command{firstCmd, secondCmd})
-
-	firstCmd = sanae.NewCommand(2, "write 4 halt", 0)
-	secondCmd = sanae.NewCommand(4, "write 8 halt", 0)
-	secondState := sanae.NewState([]sanae.Command{firstCmd, secondCmd})
-
-	algorithm := sanae.NewAlgorithm([]sanae.State{firstState, secondState})
+	reader := sanae.NewReader("algorithms/test.sasm")
+	algorithm := reader.Read()
 
 	tape := []int{1, 2, 3, 4, 5, 6, 7}
 	machine := sanae.NewMachine(tape, algorithm)
