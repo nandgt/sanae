36e353c23826389f2a83b12240dc2c2f678f0348
nandgt <93953223+nandgt@users.noreply.github.com>
2023-12-04 23:10:35 -0300

    refactor: use a struct for the tokenizer

--- src/main.rs
+++ src/main.rs
@@ -15,67 +15,83 @@ enum Tokens {
     Write(i32)
 }
 
-fn peek_token(words: &[&str], index: &mut usize) -> Tokens {
-    if *index >= words.len() {
-        return Tokens::Eof;
-    }
-    
-    let word = words[*index];
-    *index += 1;
-
-    if let Ok(number) = word.parse::<i32>() {
-        return Tokens::Operand(number);
-    }
-
-    match word {
-        "if" | "write" | "mover" | "movel" | "goto" => {
-            let next_token = peek_token(words, index);
-            if let Tokens::Operand(value) = next_token {
-                match word {
-                    "if" => return Tokens::If(value),
-                    "write" => return Tokens::Write(value),
-                    "mover" => return Tokens::Mover(value),
-                    "movel" => return Tokens::Movel(value),
-                    "goto" => return Tokens::Goto(value),
-                    _ => unreachable!(),
-                };
-            } else {
-                panic!("sanae: syntax error: must provide an operand to '{word}'");
-            }
-        },
-        "erase" =>
-            return Tokens::Erase,
-        "halt" =>
-            return Tokens::Halt,
-        _ =>
-            return Tokens::Illegal(word.to_string()),
-    }
+struct Lexer<'a> {
+    command: Vec<&'a str>,
+    pointer: usize,
 }
 
-fn tokenize(algorithm: String, index: &mut usize) -> Vec<Tokens> {
-    let words: Vec<&str> = algorithm
-        .trim()
-        .split_whitespace()
-        .collect();
-    let mut tokens: Vec<Tokens> = Vec::new();
-
-    while *index < words.len() {
-        let current_token = peek_token(&words, index);
-        match current_token {
-            Tokens::Operand(value) => panic!("sanae: syntax error: '{value}' operand is misplaced"),
-            Tokens::Eof => break,
-            Tokens::Illegal(word) => panic!("sanae: syntax error: '{word}' is not a valid instruction"),
-            _ => tokens.push(current_token),
+impl<'a> Lexer<'a> {
+    fn new(algorithm: &'a str) -> Self {
+        let words: Vec<&str> = algorithm
+            .trim()
+            .split_whitespace()
+            .collect();
+        Self {
+            command: words,
+            pointer: 0,
         }
     }
 
-    return tokens;
+    fn peek_token(&mut self) -> Tokens {
+        if self.pointer >= self.command.len() {
+            return Tokens::Eof;
+        }
+        
+        let word = self.command[self.pointer];
+        self.pointer += 1;
+    
+        if let Ok(number) = word.parse::<i32>() {
+            return Tokens::Operand(number);
+        }
+    
+        match word {
+            "if" | "write" | "mover" | "movel" | "goto" => {
+                let next_token = self.peek_token();
+                if let Tokens::Operand(value) = next_token {
+                    match word {
+                        "if" => return Tokens::If(value),
+                        "write" => return Tokens::Write(value),
+                        "mover" => return Tokens::Mover(value),
+                        "movel" => return Tokens::Movel(value),
+                        "goto" => return Tokens::Goto(value),
+                        _ => unreachable!(),
+                    };
+                } else {
+                    panic!("sanae: syntax error: must provide an operand to '{word}'");
+                }
+            },
+            "erase" =>
+                return Tokens::Erase,
+            "halt" =>
+                return Tokens::Halt,
+            _ =>
+                return Tokens::Illegal(word.to_string()),
+        }
+    }
+
+    fn tokenize(&mut self) -> Vec<Tokens> {
+        let mut tokens: Vec<Tokens> = Vec::new();
+    
+        while self.pointer < self.command.len() {
+            let current_token = self.peek_token();
+            match current_token {
+                Tokens::Operand(value) => panic!("sanae: syntax error: '{value}' operand is misplaced"),
+                Tokens::Eof => break,
+                _ => tokens.push(current_token),
+            }
+        }
+    
+        return tokens;
+    }
 }
 
+// TODO: execute algorithms from cli arguments 
+// NOTE(nand): need to find a way to "insert" memory tape into the machine, probably using something like a repl
 fn main() {
     let algorithm = fs::read_to_string("algorithms/test.sasm")
         .expect("sanae: could not read file");
-    let mut index = 0;
-    let tokens = tokenize(algorithm, &mut index);
+    
+    let mut lex = Lexer::new(&algorithm);
+    let tokens = lex.tokenize();
     println!("{:?}", tokens);
 }
